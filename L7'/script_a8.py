636189de61aef4e54fc8225d4ed6232b7ba5252a
Akshay ANAND <33004216+akshay23sep@users.noreply.github.com>
2019-08-06 23:40:20 +0200

    PyLearners

---
+++ L7'/script_a8.py
@@ -0,0 +1,10 @@
+#####################Conditions#################
+####Python uses boolean variables to identofy the conditions. The boolean values True or False are returned when expression is evaluated or compared.###
+
+###### IF & ELSE operator
+
+x = 2
+if x == 2:
+	print ("x is equal to 2")
+else:
+	print ("x is not equal to 2")
