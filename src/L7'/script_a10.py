f081f89f5658c21ab53b62119ec9e73a9f5ae3a4
Akshay ANAND <33004216+akshay23sep@users.noreply.github.com>
2019-08-06 23:41:17 +0200

    PyLearners

---
+++ src/L7'/script_a10.py
@@ -0,0 +1,7 @@
+#####################Conditions#################
+####Python uses boolean variables to identofy the conditions. The boolean values True or False are returned when expression is evaluated or compared.###
+
+###### NOT operator
+
+print (not False)
+print ((not False) ==False)
---
+++ src/L7'/script_a6.py
@@ -0,0 +1,11 @@
+#####################Conditions#################
+####Python uses boolean variables to identofy the conditions. The boolean values True or False are returned when expression is evaluated or compared.###
+
+###### AND & OR operator
+
+name = "Thomas"
+age = 24
+if name == "Thomas" and age ==24:
+	print ("Your name is Thomas and you are 24 years old.")
+if name =="Thomas" or name =="Cadario":
+	print ("Your name is either Thomas or Cadario")
